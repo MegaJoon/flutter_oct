3b49d3e8bdfcad41276f47bfce4c5cc4b344b5b3
posnix <chlrh1063@gmail.com>
2019-10-19 11:59:08 +0900

    appbar & image
    appbar & image

--- lib/1019/grocery.dart
+++ lib/1019/grocery.dart
@@ -15,31 +15,70 @@ class _GroceryState extends State<Grocery> {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          // top container
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: screenHeight * 0.30,
-            child: Stack(
-              children: <Widget>[
-                // background color
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  left: 0,
-                  bottom: 64.0,
-                  child: Container(
-                    color: _backgroundColor,
+        body: Stack(
+      children: <Widget>[
+        // top container
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: screenHeight * 0.30,
+          child: Stack(
+            children: <Widget>[
+              // background color
+              Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: 32.0,
+                child: Container(
+                  color: _backgroundColor,
+                ),
+              ),
+
+              // appbar & image
+              Positioned(
+                top: 0,
+                left: 16.0,
+                right: 16.0,
+                child: SafeArea(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                  height: 40.0,
+                  child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.arrow_back,
+                            size: 28.0,
+                            color: Colors.white,
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.search,
+                            size: 28.0,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 16.0,
+                          ),
+                          Icon(
+                            Icons.shop,
+                            size: 28.0,
+                            color: Colors.white,
+                          ),
+                        ],
                   ),
                 ),
-              ],
-            ),
+                        // image
+                        Image.asset("assets/pomegranate.png", fit: BoxFit.fitHeight, height: 300.0,),
+                      ],
+                    )),
+              )
+            ],
           ),
-        ],
-      )
-    );
+        ),
+      ],
+    ));
   }
 }
