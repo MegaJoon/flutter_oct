b969c90c01a89f19c47749e13df32e5d91c903dc
posnix <chlrh1063@gmail.com>
2019-10-25 12:37:35 +0900

    pages[currentIndex]
    pages[currentIndex]

--- lib/1025/knights.dart
+++ lib/1025/knights.dart
@@ -11,19 +11,29 @@ class KnightsApp extends StatefulWidget {
 
 class _KnightsAppState extends State<KnightsApp> {
   Color _color = Color.fromRGBO(136, 123, 121, 1);
-  int currentIndex = 0;
+  int currentIndex = 1;
+  List<Widget> pages;
 
-  void _onTab(int index){
+  void _onTab(int index) {
     setState(() {
       currentIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
+    _definePages();
     return Scaffold(
       body: Stack(
         children: <Widget>[
           // main
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 64.0,
+            child: pages[currentIndex],
+          ),
 
           // bottombar
           Positioned(
@@ -37,10 +47,38 @@ class _KnightsAppState extends State<KnightsApp> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  BottomTab(iconData: Icons.home, text: "Home", isSelected: currentIndex ==0, onTab: (){_onTab(0);},),
-                  BottomTab(iconData: Icons.menu, text: "Shop", isSelected: currentIndex ==1, onTab: (){_onTab(1);},),
-                  BottomTab(iconData: Icons.bookmark_border, text: "Mark", isSelected: currentIndex ==2, onTab: (){_onTab(2);},),
-                  BottomTab(iconData: Icons.person_outline, text: "User", isSelected: currentIndex ==3, onTab: (){_onTab(3);},),
+                  BottomTab(
+                    iconData: Icons.home,
+                    text: "Home",
+                    isSelected: currentIndex == 0,
+                    onTab: () {
+                      _onTab(0);
+                    },
+                  ),
+                  BottomTab(
+                    iconData: Icons.menu,
+                    text: "Shop",
+                    isSelected: currentIndex == 1,
+                    onTab: () {
+                      _onTab(1);
+                    },
+                  ),
+                  BottomTab(
+                    iconData: Icons.bookmark_border,
+                    text: "Mark",
+                    isSelected: currentIndex == 2,
+                    onTab: () {
+                      _onTab(2);
+                    },
+                  ),
+                  BottomTab(
+                    iconData: Icons.person_outline,
+                    text: "User",
+                    isSelected: currentIndex == 3,
+                    onTab: () {
+                      _onTab(3);
+                    },
+                  ),
                 ],
               ),
             ),
@@ -48,5 +86,14 @@ class _KnightsAppState extends State<KnightsApp> {
         ],
       ),
     );
+  }
+
+  _definePages(){
+    pages = [
+      Placeholder(color: Colors.red,),
+      Placeholder(color: Colors.amber,),
+      Placeholder(color: Colors.black,),
+      Placeholder(color: Colors.indigo,),
+    ];
   }
 }
