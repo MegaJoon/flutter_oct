93101654e3fd0f1080c4c251bf15c2d2b80b53d9
posnix <chlrh1063@gmail.com>
2019-10-25 12:15:52 +0900

    KnightsApp
    KnightsApp

---
+++ lib/1025/knights.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// https://dribbble.com/shots/7807406-Knights-shop/attachments/463368?mode=media
+
+class KnightsApp extends StatefulWidget {
+  @override
+  _KnightsAppState createState() => _KnightsAppState();
+}
+
+class _KnightsAppState extends State<KnightsApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          //
+        ],
+      ),
+    );
+  }
+}
