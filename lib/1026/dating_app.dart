eee167160cddadd07485bd74b2e7b458e9949dc8
posnix <chlrh1063@gmail.com>
2019-10-26 15:25:40 +0900

    fontfamily
    fontfamily

--- lib/1026/dating_app.dart
+++ lib/1026/dating_app.dart
@@ -8,8 +8,25 @@ class DatingApp extends StatefulWidget {
 }
 
 class _DatingAppState extends State<DatingApp> {
+  String title = "Matches";
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          // abbbar
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            actions: <Widget>[
+              Icon(Icons.filter_list, size: 28.0, color: Colors.black,),
+            ],
+            bottom: PreferredSize(
+                child: Text(title, style: TextStyle(color: Colors.black, fontSize: 28.0, fontWeight: FontWeight.bold, fontFamily: "Bannie"),),
+                preferredSize: Size.fromHeight(8.0)),
+          )
+        ],
+      ),
+
+    );
   }
 }
