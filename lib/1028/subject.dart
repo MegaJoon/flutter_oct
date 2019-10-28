31ab13297672ad4108014b4c731ef654c59fd102
posnix <chlrh1063@gmail.com>
2019-10-28 18:05:13 +0900

    appbar
    appbar

--- lib/1028/subject.dart
+++ lib/1028/subject.dart
@@ -10,6 +10,37 @@ class SubjectApp extends StatefulWidget {
 class _SubjectAppState extends State<SubjectApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          // appbar
+          SafeArea(
+            top: true,
+            left: true,
+            right: true,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              height: 60.0,
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Subject", style: TextStyle(color: Colors.black, fontSize: 32.0, fontWeight: FontWeight.w600),),
+                      RichText(text: TextSpan(children: [
+                        TextSpan(text: "Today's English porgress", style: TextStyle(color: Colors.grey, fontSize: 12.0, fontWeight: FontWeight.w600)),
+                        TextSpan(text: "15 min", style: TextStyle(color: Colors.teal, fontSize: 14.0, fontWeight: FontWeight.w600))
+                      ]),)
+                    ],
+                  ),
+                  Spacer(),
+                  Icon(Icons.border_color, color: Colors.black, size: 20.0,),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
