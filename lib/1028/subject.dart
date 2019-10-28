9c84997a1714127cc5eed9ad4e93541ca87a226c
posnix <chlrh1063@gmail.com>
2019-10-28 18:12:15 +0900

    bottom
    bottom

--- lib/1028/subject.dart
+++ lib/1028/subject.dart
@@ -8,10 +8,62 @@ class SubjectApp extends StatefulWidget {
 }
 
 class _SubjectAppState extends State<SubjectApp> {
+  List<Widget> pages;
+
+  // bottom index
+  int _currentIndex = 1;
+
   @override
   Widget build(BuildContext context) {
+    _definePages();
+
     return Scaffold(
-      body: Column(
+      body: pages[_currentIndex],
+
+      // bottom
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (idx) {
+            setState(() {
+              _currentIndex = idx;
+            });
+          },
+          showUnselectedLabels: true,
+          showSelectedLabels: true,
+          selectedIconTheme: IconThemeData(color: Colors.teal, size: 28.0),
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+          selectedFontSize: 16.0,
+          selectedItemColor: Colors.teal,
+          unselectedIconTheme: IconThemeData(color: Colors.grey, size: 26.0),
+          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w400),
+          unselectedFontSize: 14.0,
+          unselectedItemColor: Colors.grey,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Home"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.subject),
+              title: Text("Subject"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.whatshot),
+              title: Text("Growing"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              title: Text("Profile"),
+            ),
+          ]),
+    );
+  }
+
+  _definePages(){
+    pages = [
+      Placeholder(),
+      // initial page
+      Column(
         children: <Widget>[
           // appbar
           SafeArea(
@@ -26,21 +78,33 @@ class _SubjectAppState extends State<SubjectApp> {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text("Subject", style: TextStyle(color: Colors.black, fontSize: 32.0, fontWeight: FontWeight.w600),),
-                      RichText(text: TextSpan(children: [
-                        TextSpan(text: "Today's English porgress", style: TextStyle(color: Colors.grey, fontSize: 12.0, fontWeight: FontWeight.w600)),
-                        TextSpan(text: "15 min", style: TextStyle(color: Colors.teal, fontSize: 14.0, fontWeight: FontWeight.w600))
-                      ]),)
+                      Text(
+                        "Subject",
+                        style: TextStyle(color: Colors.black, fontSize: 32.0, fontWeight: FontWeight.w600),
+                      ),
+                      RichText(
+                        text: TextSpan(children: [
+                          TextSpan(
+                              text: "Today's English porgress", style: TextStyle(color: Colors.grey, fontSize: 12.0, fontWeight: FontWeight.w600)),
+                          TextSpan(text: "15 min", style: TextStyle(color: Colors.teal, fontSize: 14.0, fontWeight: FontWeight.w600))
+                        ]),
+                      )
                     ],
                   ),
                   Spacer(),
-                  Icon(Icons.border_color, color: Colors.black, size: 20.0,),
+                  Icon(
+                    Icons.border_color,
+                    color: Colors.black,
+                    size: 20.0,
+                  ),
                 ],
               ),
             ),
           )
         ],
       ),
-    );
+      Placeholder(),
+      Placeholder(),
+    ];
   }
 }
